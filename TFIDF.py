eb72f27ad9baef2d00373bc30937329e83bcbea3
Zahraheidari1 <zahra2019.iran@gmail.com>
2023-05-23 11:11:06 +0430

    tf

---
+++ TFIDF.py
@@ -0,0 +1,7 @@
+from sklearn.metrics.pairwise import cosine_similarity
+import hazm
+from hazm.utils import stopwords_list
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def tf_idf(documents):
+    pass
